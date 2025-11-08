28354c473cf4388bdb8131ca9fa28e7802a0d987
SeongJeGyeong <cmjegyeong@Gmail.com>
2025-11-08 12:06:24 +0900

    feat(DSG): add animation state enum

---
+++ Assets/Scripts/Stage/DSG/Enums/AnimStateType.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace LUP.DSG.Utils.Enums
+{
+    public enum EAnimStateType
+    {
+        Idle,
+        Dash_Fwd,
+        Dash_Bwd,
+        Attack_Melee,
+        Attack_Range,
+        Hitted,
+        Died,
+    }
+}
