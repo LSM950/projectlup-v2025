b4983fa72f0d353d7990955059c6d18360ba6a69
KimHyunJun <proress@naver.com>
2025-12-13 11:45:33 +0900

    feat(FW) : Refactory RLInventory

--- Assets/2_Scripts/-Stage/RL/RougelikeStage.cs
+++ Assets/2_Scripts/-Stage/RL/RougelikeStage.cs
@@ -1,8 +1,6 @@
-﻿using LUP.ES;
-using LUP.RL;
+﻿using LUP.RL;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace LUP
 {
@@ -16,14 +14,10 @@ namespace LUP
 
         public Inventory inventory;
 
-       
-
         protected override void Awake() 
         {
             base.Awake();
             StageKind = Define.StageKind.RL;
-
-            inventory.filename = "RLinventory.json";
         }
 
         void Start()
@@ -39,34 +33,8 @@ namespace LUP
         public override IEnumerator OnStageEnter()
         {
             yield return base.OnStageEnter();
-            
-            // Inventory 생성 및 파일명 설정
-            string inventoryFilename = inventory.filename;
 
-            if (JsonDataHelper.FileExists(inventoryFilename))
-            {
-                // 기존 인벤토리 로드
-                inventory = JsonDataHelper.LoadData<Inventory>(inventoryFilename);
-                if (inventory != null)
-                {
-                    inventory.filename = inventoryFilename;
-                    inventory.InitializeFromJson();  // Dictionary 복원
-                    Debug.Log("[MainStage] 인벤토리 로드 완료");
-                }
-                else
-                {
-                    Debug.LogWarning("[MainStage] 인벤토리 로드 실패, 새로 생성");
-                    inventory = new Inventory();
-                    inventory.filename = inventoryFilename;
-                }
-            }
-            else
-            {
-                // 새 인벤토리 생성
-                inventory = new Inventory();
-                inventory.filename = inventoryFilename;
-                Debug.Log("[MainStage] 새 인벤토리 생성");
-            }
+            inventory = InventoryManager.Instance.LoadOrCreateInventory("RL", "RLinventory.json");
 
             if (lobbyGameCenter != null)
             {
