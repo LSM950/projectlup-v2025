0aedd0487a0eab07f480e525db4b9973afe37ce3
SeongJeGyeong <cmjegyeong@gmail.com>
2026-02-19 12:04:58 +0900

    feat(DSG): modify attribute icon

--- Assets/2_Scripts/Games/DSG/UI/AttributeIconContainer.cs
+++ Assets/2_Scripts/Games/DSG/UI/AttributeIconContainer.cs
@@ -5,34 +5,17 @@ using UnityEngine;
 
 namespace LUP.DSG
 {
-    [Serializable]
-    public struct AttributeTypeImage
-    {
-        [SerializeField]
-        public EAttributeType attributeType;
-        [SerializeField]
-        public Sprite typeIcon;
-        [SerializeField]
-        public UnityEngine.Color typeColor;
-        public AttributeTypeImage(EAttributeType type, Sprite icon, UnityEngine.Color color)
-        {
-            attributeType = type;
-            typeIcon = icon;
-            typeColor = color;
-        }
-    }
-
     public class AttributeIconContainer : MonoBehaviour
     {
         [SerializeField]
-        private List<AttributeTypeImage> typeList = new List<AttributeTypeImage>();
+        private AttributeData attributeData;
 
         [SerializeField]
         public Dictionary<EAttributeType, AttributeTypeImage> attributeIconDictionary = new Dictionary<EAttributeType, AttributeTypeImage>();
 
         private void Awake()
         {
-            foreach(AttributeTypeImage type in typeList)
+            foreach(AttributeTypeImage type in attributeData.attributeList)
             {
                 attributeIconDictionary[type.attributeType] = type;
             }
